fbbbdf896913da921d63baa33968ba0fec1c99c8
LHHT-DISCOVERY <lytri102@gmail.com>
2024-06-12 09:32:48 +0700

    config CORS for api gateway

--- api-gateway/src/main/java/com/example/gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/example/gateway/configuration/WebClientConfiguration.java
@@ -3,9 +3,14 @@ package com.example.gateway.configuration;
 import com.example.gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 // because using @PostExchange in repo, not using FeignClient ,so need this class to can call to server identity
@@ -22,5 +27,17 @@ public class WebClientConfiguration {
                 .builderFor(WebClientAdapter.create(webClient)).build();
 
         return httpServiceProxyFactory.createClient(IdentityClient.class);
+    }
+
+    // configuration for cros for api gateway
+    @Bean
+    CorsWebFilter corsWebFilter() { // using web core "reactive"
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
