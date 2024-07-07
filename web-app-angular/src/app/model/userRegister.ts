9ede47d9e54c076421a286e9f571a0a7053a9bb6
LHHT-DISCOVERY <lytri102@gmail.com>
2024-07-07 12:42:47 +0700

    add more signup in angular

---
+++ web-app-angular/src/app/model/userRegister.ts
@@ -0,0 +1,11 @@
+export interface userRegister {
+    username?: String;
+    password: String;
+    firstName: String;
+    lastName: String;
+    dob: String;
+    city: String;
+    gender: String;
+    email: String;
+    roles: String[]
+}
